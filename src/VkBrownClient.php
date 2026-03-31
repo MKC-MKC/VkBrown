7e54cabecc8f5400889cb7d2a1a2342621e2b4b6
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-03-31 16:58:39 +0300

    Forgotten operator

--- src/VkBrownClient.php
+++ src/VkBrownClient.php
@@ -16,7 +16,7 @@ class VkBrownClient extends VkBrownClientAbstract
 		if (empty($response = file_get_contents(filename: "php://input"))) return [];
 
 		$decoded = json_decode($response, true);
-		if (json_last_error() != JSON_ERROR_NONE) {
+		if (json_last_error() !== JSON_ERROR_NONE) {
 			if (self::$debug) error_log("json_last_error: " . json_last_error());
 			if (self::$debug) error_log("json_last_error_msg: " . json_last_error_msg());
 			return [];
