40365da166754d84d0007e4b53546e037ecbd74f
RAZIEL KOSTELLO <rkostello@haikiri.ru>
2026-03-31 14:25:46 +0300

    Добил PHPDoc для route-методов VK

--- src/Route/DocsRoute.php
+++ src/Route/DocsRoute.php
@@ -7,26 +7,56 @@ namespace Haikiri\VkBrown\Route;
 class DocsRoute extends AbstractRoute
 {
 
+	/**
+	 * Метод возвращает документы по их идентификаторам.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function getById(array $params = []): mixed
 	{
 		return $this->request("docs.getById", $params, ["docs"]);
 	}
 
+	/**
+	 * Метод возвращает upload-сервер для отправки документа в сообщения.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function getMessagesUploadServer(array $params = []): mixed
 	{
 		return $this->request("docs.getMessagesUploadServer", $params);
 	}
 
+	/**
+	 * Метод возвращает upload-сервер для загрузки документа на стену.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function getWallUploadServer(array $params = []): mixed
 	{
 		return $this->request("docs.getWallUploadServer", $params);
 	}
 
+	/**
+	 * Метод сохраняет загруженный документ.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function save(array $params = []): mixed
 	{
 		return $this->request("docs.save", $params, ["tags"]);
 	}
 
+	/**
+	 * Метод выполняет поиск документов.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function search(array $params = []): mixed
 	{
 		return $this->request("docs.search", $params);
--- src/Route/WallRoute.php
+++ src/Route/WallRoute.php
@@ -7,16 +7,34 @@ namespace Haikiri\VkBrown\Route;
 class WallRoute extends AbstractRoute
 {
 
+	/**
+	 * Метод закрывает комментарии к записи на стене.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function closeComments(array $params = []): mixed
 	{
 		return $this->request("wall.closeComments", $params);
 	}
 
+	/**
+	 * Метод создаёт комментарий к записи на стене.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function createComment(array $params = []): mixed
 	{
 		return $this->request("wall.createComment", $params);
 	}
 
+	/**
+	 * Метод открывает комментарии к записи на стене.
+	 *
+	 * @param array $params
+	 * @return mixed
+	 */
 	public function openComments(array $params = []): mixed
 	{
 		return $this->request("wall.openComments", $params);
